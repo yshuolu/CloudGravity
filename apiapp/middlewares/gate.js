d830469e1733596968ce579a65165d2fcc3a4fa4
Phineas-z <yshuolu@gmail.com>
2014-06-29 23:18:20 -0400

    clean the trash code of billing function

--- apiapp/middlewares/gate.js
+++ apiapp/middlewares/gate.js
@@ -64,60 +64,6 @@ exports.appAuth = function(){
 }
 
 /**
- * Middleware for billing system.
- * Record the api consumption in the predefined time interval (billing interval).
- */
-
-// exports.consume = function(){
-// 	return function(req, res, next){
-// 		if (!req.app.billing){			
-// 			//no billing object
-// 			return setDefaultBilling(req, res, next);
-
-// 		}else if ( !isBillingExpired(req.app.billing, req.query.timestamp) ){
-// 			//current attached billing is valid, check if the access is under limit
-// 			if (req.app.billing.consumption >= config.consumptionLimit[req.app.billing.level]){
-// 				return next('Run out of api access');
-// 			}
-
-// 			req.app.billing.incrementConsumption(1, function(err){
-// 				return next(err);
-// 			});
-
-// 		}else if(req.app.billing.level === 0){
-// 			//current attached default billing expired, reset it
-// 			return setDefaultBilling(req, res, next); 
-
-// 		}else{
-// 			//current high level billing expired, iterate the billing list and try to find a valid one 
-// 			findValidBilling(req.app.billing, req.query.timestamp, function(err, validBilling){
-// 				if (err) return next(err);
-
-// 				if (!validBilling){
-// 					//currently the app has not a valid high level billing, fall back to default billing
-// 					return setDefaultBilling(req, res, next);
-
-// 				}else{
-// 					//find a new valid billing object in high level billing link list
-// 					//attach this billing object to app
-// 					req.app.billing = validBilling;
-
-// 					//increment this billing
-// 					validBilling.incrementConsumption(1, function(err){
-// 						if (err) return next(err);
-
-// 						//save the app billing field
-// 						req.app.save(function(err, app){
-// 							next(err);
-// 						});
-// 					});
-// 				}
-// 			});
-// 		}
-// 	}
-// }
-
-/**
  * Middleware for api statistics.
  * Record the meta data about api access.
  * For example, record which app use which api and the count.
@@ -171,7 +117,7 @@ exports.bill = function(){
 			getValidPlan(req, req.query.timestamp, function(err, plan){
 				if (err) return next(err);
 
-				req.app.plan = plan;
+				req.app.plan = plan._id;
 
 				req.app.save(function(err){
 					if (err) return next(err);
@@ -231,103 +177,6 @@ function verifySignature(req){
 	//compare the signatures
 	return (signature === req.query.signature)
 }
-
-
-/**
- * Local function to create the default billing interval:
- * lowest level, price is 0;
- * start is now;
- * end is the end of day (China timezone);
- */
-
-// function defaultBilling(timestamp){
-
-// 	var billingStartDate = new Date();
-// 	var billingExpireDate = new Date( billingStartDate.getTime() + 1 * 24 * 60 * 60 * 1000 ); // one day
-
-// 	var defaultBilling = Billing({
-// 		start: billingStartDate,
-// 		expire: billingExpireDate,
-// 		level: 0,
-// 		consumption: 1
-// 	});
-
-// 	return defaultBilling;
-// }
-
-/**
- * Local function acting as a middleware funtion.
- * It set default billing (level 0) for the app.
- * Every app can only have at most 1 default billing object, but may have multiple other level
- * billing object.
- * So if:
- * 1. the app already have old default billing object, reset the default billing object;
- * 2. the app has not created any billing object yet, create a new default billing object for it;
- * 3. the app has old high level billing object, create a new default billing object for it.
- */
-// function setDefaultBilling(req, res, next){
-// 	var billingStartDate = new Date();
-// 	var billingExpireDate = new Date( billingStartDate.getTime() + 1 * 24 * 60 * 60 * 1000 ); // one day
-
-// 	var newBilling = req.app.billing;
-
-// 	if (newBilling && newBilling.level === 0){
-// 		//old billing is default billing, just reset it
-// 		newBilling.start = billingStartDate;
-// 		newBilling.expire = billingExpireDate;
-// 		newBilling.consumption = consumption;
-
-// 	}else{
-// 		//old billing not exists, or it is not default billing, 
-// 		//should create a new default billing object
-// 		newBilling = Billing({
-// 			start: billingStartDate,
-// 			expire: billingExpireDate,
-// 			level: 0,
-// 			consumption: 1
-// 		});
-// 	}
-
-// 	//double direction reference
-// 	newBilling.app = req.app._id;
-// 	req.app.billing = newBilling._id;
-
-// 	//save billing object
-// 	newBilling.save(function(err, billing){
-// 		if (err) return next(err);
-
-// 		//update app object's billing field
-// 		req.app.save(function(err, app){
-// 			return next(err);
-// 		});
-// 	});
-// }
-
-// //
-// function findValidBilling(billing, timestamp, fn){
-// 	if ( isBillingExpired(billing, timestamp) ){
-// 		//this high leve billing is expired, need to search the next high level billing object
-// 		billing.populate('next', function(err, billing){
-// 			//if populate failed, or populated object is null, then return directly
-// 			if (err || !billing.next) return fn(err);
-
-// 			findValidBilling(billing.next, timestamp, fn);
-// 		});
-
-// 	}else{
-// 		//the billing is just valid
-// 		//err is null
-// 		fn(null, billing);
-// 	}
-// }
-
-// function isBillingExpired(billing, timestamp){
-// 	if ( billing.expire.getTime() / 1000 <= timestamp ){
-// 		return true;
-// 	}
-
-// 	return false;
-// }
 
 /**
  * Local function to save an array of documents at once.
